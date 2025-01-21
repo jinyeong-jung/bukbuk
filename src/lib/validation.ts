d499606e36aec8e0978f3b0bf4450b2a6694f340
Jinyeong Jung <jinyeong32@gmail.com>
2025-01-21 14:57:40 +0900

    Lucia SignUp & Login (Frontend)

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const requiredString = z.string().trim().min(1, "Field is required");
+const requiredString = z.string().trim().min(1, "This field is required");
 
 export const signUpSchema = z.object({
   email: requiredString.email("Invalid email address"),
