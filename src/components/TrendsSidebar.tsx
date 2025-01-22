62af3c1f6868814a7579550b29680237be009c4a
Jinyeong Jung <jinyeong32@gmail.com>
2025-01-22 17:28:56 +0900

    Trending topics sidebar - Follow (Suspense)

---
+++ src/components/TrendsSidebar.tsx
@@ -0,0 +1,59 @@
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { validateRequest } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { userDataSelect } from "@/lib/types";
+import UserAvatar from "@/components/UserAvatar";
+import { Button } from "@/components/ui/button";
+
+export default function TrendsSidebar() {
+  return (
+    <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
+      <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+        <WhoToFollow />
+      </Suspense>
+    </div>
+  );
+}
+
+async function WhoToFollow() {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const usersToFollow = await prisma.user.findMany({
+    where: {
+      NOT: {
+        id: user.id,
+      },
+    },
+    select: userDataSelect,
+    take: 5,
+  });
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Who to follow</div>
+      {usersToFollow.map((user) => (
+        <div key={user.id} className="flex items-center justify-between gap-3">
+          <Link
+            href={`/users/${user.username}`}
+            className="flex items-center gap-3"
+          >
+            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+            <div>
+              <p className="line-clamp-1 break-all font-semibold hover:underline">
+                {user.displayName}
+              </p>
+              <p className="line-clamp-1 break-all text-sm text-muted-foreground">
+                @{user.username}
+              </p>
+            </div>
+          </Link>
+          <Button>Follow</Button>
+        </div>
+      ))}
+    </div>
+  );
+}
