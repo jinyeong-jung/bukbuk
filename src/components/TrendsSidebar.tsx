176b02c74a28c330bcaf6d9aca3352a7ef5c8da0
Jinyeong Jung <jinyeong32@gmail.com>
2025-01-22 18:04:58 +0900

    Hashtag sidebar (unstable_cache)

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -6,12 +6,15 @@ import { prisma } from "@/lib/prisma";
 import { userDataSelect } from "@/lib/types";
 import UserAvatar from "@/components/UserAvatar";
 import { Button } from "@/components/ui/button";
+import { unstable_cache } from "next/cache";
+import { formatNumber } from "@/lib/utils";
 
 export default function TrendsSidebar() {
   return (
     <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <WhoToFollow />
+        <TrendingTopics />
       </Suspense>
     </div>
   );
@@ -54,6 +57,53 @@ async function WhoToFollow() {
           <Button>Follow</Button>
         </div>
       ))}
+    </div>
+  );
+}
+
+const getTrendingTopics = unstable_cache(
+  async () => {
+    const result = await prisma.$queryRaw<{ hashtag: string; count: bigint }[]>`
+        SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hashtag, COUNT(*) AS count
+        FROM posts
+        GROUP BY (hashtag)
+        ORDER BY count DESC, hashtag ASC
+        LIMIT 5
+    `;
+
+    return result.map((row) => ({
+      hashtag: row.hashtag,
+      count: Number(row.count),
+    }));
+  },
+  ["trending_topics"],
+  {
+    revalidate: 3 * 60 * 60,
+  },
+);
+
+async function TrendingTopics() {
+  const trendingTopics = await getTrendingTopics();
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Trending topics</div>
+      {trendingTopics.map(({ hashtag, count }) => {
+        const title = hashtag.split("#")[1];
+        return (
+          <Link key={title} href={`/hashtag/${title}`} className="block">
+            <p
+              className="line-clamp-1 break-all font-semibold text-primary hover:underline"
+              title={hashtag}
+            >
+              {hashtag}
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {formatNumber(count)} {count === 1 ? "post" : "posts"}
+            </p>
+          </Link>
+        );
+      })}
     </div>
   );
 }
