711de5fefb45ae7633865d887efe308791ea2c45
Jinyeong Jung <jinyeong32@gmail.com>
2025-01-31 16:47:53 +0900

    Follow feature (React Query optimistic updates)

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -3,11 +3,11 @@ import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { validateRequest } from "@/auth";
 import { prisma } from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
 import UserAvatar from "@/components/UserAvatar";
-import { Button } from "@/components/ui/button";
 import { unstable_cache } from "next/cache";
 import { formatNumber } from "@/lib/utils";
+import FollowButton from "@/components/FollowButton";
+import { getUserDataSelect } from "@/lib/types";
 
 export default function TrendsSidebar() {
   return (
@@ -30,8 +30,13 @@ async function WhoToFollow() {
       NOT: {
         id: user.id,
       },
+      followers: {
+        none: {
+          followerId: user.id,
+        },
+      },
     },
-    select: userDataSelect,
+    select: getUserDataSelect(user.id),
     take: 5,
   });
 
@@ -54,7 +59,15 @@ async function WhoToFollow() {
               </p>
             </div>
           </Link>
-          <Button>Follow</Button>
+          <FollowButton
+            userId={user.id}
+            initialState={{
+              followers: user._count.followers,
+              isFollowedByUser: user.followers.some(
+                ({ followerId }) => followerId === user.id,
+              ),
+            }}
+          />
         </div>
       ))}
     </div>
