bf8281fbbc18b3905763665dff4d3f582db4077f
Jinyeong Jung <jinyeong32@gmail.com>
2025-02-05 22:05:56 +0900

    Comments feature (with nifinite loading)

---
+++ src/components/comments/CommentInput.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import { Loader2, SendHorizonal } from "lucide-react";
+import { PostData } from "@/lib/types";
+import { useSubmitCommentMutation } from "@/components/comments/mutations";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface CommentInputProps {
+  post: PostData;
+}
+
+export default function CommentInput({ post }: CommentInputProps) {
+  const [input, setInput] = useState("");
+
+  const mutation = useSubmitCommentMutation(post.id);
+
+  async function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!input) return;
+
+    mutation.mutate(
+      {
+        post,
+        content: input,
+      },
+      {
+        onSuccess: () => setInput(""),
+      },
+    );
+  }
+
+  return (
+    <form className="flex w-full items-center gap-2" onSubmit={onSubmit}>
+      <Input
+        placeholder="White a comment..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        autoFocus
+      />
+      <Button
+        type="submit"
+        variant="ghost"
+        size="icon"
+        disabled={!input.trim() || mutation.isPending}
+      >
+        {!mutation.isPending ? (
+          <SendHorizonal />
+        ) : (
+          <Loader2 className="animate-spin" />
+        )}
+      </Button>
+    </form>
+  );
+}
---
+++ src/components/comments/CommentMoreButton.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { MoreHorizontal, Trash2Icon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { CommentData } from "@/lib/types";
+import DeleteCommentDialog from "./DeleteCommentDialog";
+
+interface CommentMoreButtonProps {
+  comment: CommentData;
+  className?: string;
+}
+
+export default function CommentMoreButton({
+  comment,
+  className,
+}: CommentMoreButtonProps) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost" className={className}>
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
+            <span className="flex items-center gap-3 text-destructive">
+              <Trash2Icon className="size-4" />
+              Delete
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteCommentDialog
+        comment={comment}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </>
+  );
+}
