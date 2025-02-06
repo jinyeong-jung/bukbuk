ada8a228cc4cae4b48b4ae7c9fb2742f0cbd73ea
Jinyeong Jung <jinyeong32@gmail.com>
2025-02-06 15:33:22 +0900

    Notifications feature (Prisma transactions & ReactQuery refetchInterval

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -1,12 +1,26 @@
-import { Bell, Bookmark, Home, Mail } from "lucide-react";
+import { Bookmark, Home, Mail } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import NotificationsButton from "./NotificationsButton";
+import { validateRequest } from "@/auth";
+import { prisma } from "@/lib/prisma";
 
 interface MenuBarProps {
   className?: string;
 }
 
-export default function MenuBar({ className }: MenuBarProps) {
+export default async function MenuBar({ className }: MenuBarProps) {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const unreadNotificationsCount = await prisma.notification.count({
+    where: {
+      recipientId: user.id,
+      read: false,
+    },
+  });
+
   return (
     <div className={className}>
       <Button
@@ -21,17 +35,9 @@ export default function MenuBar({ className }: MenuBarProps) {
         </Link>
       </Button>
 
-      <Button
-        variant="ghost"
-        className="flex items-center justify-start gap-3"
-        title="Notifications"
-        asChild
-      >
-        <Link href="/notifications">
-          <Bell />
-          <span className="hidden lg:inline">Notifications</span>
-        </Link>
-      </Button>
+      <NotificationsButton
+        initialState={{ unreadCount: unreadNotificationsCount }}
+      />
 
       <Button
         variant="ghost"
---
+++ src/app/(main)/NotificationsButton.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Bell } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
+import { NotificationCountInfo } from "@/lib/types";
+import kyInstance from "@/lib/ky";
+
+interface NotificationsButtonProps {
+  initialState: NotificationCountInfo;
+}
+
+export default function NotificationsButton({
+  initialState,
+}: NotificationsButtonProps) {
+  const { data } = useQuery({
+    queryKey: ["unread-notification-count"],
+    queryFn: () =>
+      kyInstance
+        .get("/api/notifications/unread-count")
+        .json<NotificationCountInfo>(),
+    initialData: initialState,
+    refetchInterval: 60 * 1000,
+  });
+
+  return (
+    <Button
+      variant="ghost"
+      className="flex items-center justify-start gap-3"
+      title="Notifications"
+      asChild
+    >
+      <Link href="/notifications">
+        <div className="relative">
+          <Bell />
+          {!!data.unreadCount && (
+            <span className="absolute -right-2 -top-2 rounded-full bg-primary px-1 text-xs font-medium tabular-nums text-primary-foreground">
+              {data.unreadCount}
+            </span>
+          )}
+        </div>
+        <span className="hidden lg:inline">Notifications</span>
+      </Link>
+    </Button>
+  );
+}
